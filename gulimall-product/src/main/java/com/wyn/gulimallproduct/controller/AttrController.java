fee58f36f9e0ad36c9d2e9cf142c06bc5e676bbc
wei-ya-nan <soyongman0@gmail.com>
2022-08-04 23:34:01 +0800

    商品系统-平台属性-属性分组-新增属性优化

--- gulimall-product/src/main/java/com/wyn/gulimallproduct/controller/AttrController.java
+++ gulimall-product/src/main/java/com/wyn/gulimallproduct/controller/AttrController.java
@@ -1,22 +1,15 @@
 package com.wyn.gulimallproduct.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-
-import com.wyn.gulimallproduct.vo.AttrVo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wyn.gulimallproduct.entity.AttrEntity;
-import com.wyn.gulimallproduct.service.AttrService;
 import com.wyn.common.utils.PageUtils;
 import com.wyn.common.utils.R;
+import com.wyn.gulimallproduct.entity.AttrEntity;
+import com.wyn.gulimallproduct.service.AttrService;
+import com.wyn.gulimallproduct.vo.AttrVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
@@ -32,12 +25,18 @@ public class AttrController {
     @Autowired
     private AttrService attrService;
 
+    @GetMapping("/base/list/{catelogId}")
+    public R baseAttrList(@RequestParam Map<String, Object> params, @PathVariable Long catelogId) {
+        PageUtils page = attrService.queryBaseAttrpPage(params, catelogId);
+        return R.ok().put("page", page);
+    }
+
     /**
      * 列表
      */
     @RequestMapping("/list")
     //@RequiresPermissions("gulimallproduct:attr:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = attrService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -49,8 +48,8 @@ public class AttrController {
      */
     @RequestMapping("/info/{attrId}")
     //@RequiresPermissions("gulimallproduct:attr:info")
-    public R info(@PathVariable("attrId") Long attrId){
-		AttrEntity attr = attrService.getById(attrId);
+    public R info(@PathVariable("attrId") Long attrId) {
+        AttrEntity attr = attrService.getById(attrId);
 
         return R.ok().put("attr", attr);
     }
@@ -60,8 +59,8 @@ public class AttrController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("gulimallproduct:attr:save")
-    public R save(@RequestBody AttrVo attr){
-		attrService.saveAttr(attr);
+    public R save(@RequestBody AttrVo attr) {
+        attrService.saveAttr(attr);
 
         return R.ok();
     }
@@ -71,8 +70,8 @@ public class AttrController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("gulimallproduct:attr:update")
-    public R update(@RequestBody AttrEntity attr){
-		attrService.updateById(attr);
+    public R update(@RequestBody AttrEntity attr) {
+        attrService.updateById(attr);
 
         return R.ok();
     }
@@ -82,8 +81,8 @@ public class AttrController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("gulimallproduct:attr:delete")
-    public R delete(@RequestBody Long[] attrIds){
-		attrService.removeByIds(Arrays.asList(attrIds));
+    public R delete(@RequestBody Long[] attrIds) {
+        attrService.removeByIds(Arrays.asList(attrIds));
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/wyn/gulimallproduct/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/wyn/gulimallproduct/service/impl/AttrServiceImpl.java
@@ -1,31 +1,42 @@
 package com.wyn.gulimallproduct.service.impl;
 
-import com.wyn.gulimallproduct.dao.AttrAttrgroupRelationDao;
-import com.wyn.gulimallproduct.entity.AttrAttrgroupRelationEntity;
-import com.wyn.gulimallproduct.entity.AttrGroupEntity;
-import com.wyn.gulimallproduct.vo.AttrVo;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wyn.common.utils.PageUtils;
 import com.wyn.common.utils.Query;
-
+import com.wyn.gulimallproduct.dao.AttrAttrgroupRelationDao;
 import com.wyn.gulimallproduct.dao.AttrDao;
+import com.wyn.gulimallproduct.dao.AttrGroupDao;
+import com.wyn.gulimallproduct.dao.CategoryDao;
+import com.wyn.gulimallproduct.entity.AttrAttrgroupRelationEntity;
 import com.wyn.gulimallproduct.entity.AttrEntity;
+import com.wyn.gulimallproduct.entity.AttrGroupEntity;
+import com.wyn.gulimallproduct.entity.CategoryEntity;
 import com.wyn.gulimallproduct.service.AttrService;
+import com.wyn.gulimallproduct.vo.AttrRespVo;
+import com.wyn.gulimallproduct.vo.AttrVo;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
     @Resource
     AttrAttrgroupRelationDao relationDao;
+
+    @Resource
+    AttrGroupDao attrGroupDao;
+
+    @Resource
+    CategoryDao categoryDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -40,7 +51,7 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
     @Override
     @Transactional
     public void saveAttr(AttrVo attr) {
-        AttrEntity attrEntity  = new AttrEntity();
+        AttrEntity attrEntity = new AttrEntity();
         BeanUtils.copyProperties(attr, attrEntity);
         // 保存基本数据
         this.save(attrEntity);
@@ -51,6 +62,50 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         relationDao.insert(relationEntity);
 
 
+    }
+
+    @Override
+    public PageUtils queryBaseAttrpPage(Map<String, Object> params, Long catelogId) {
+        QueryWrapper<AttrEntity> queryWrapper = new QueryWrapper<>();
+        if (catelogId != 0) {
+            queryWrapper.eq("catelog_id", catelogId);
+        }
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            queryWrapper.and((wrapper -> {
+                wrapper.eq("attr_id", key).or().like("attr_name", key);
+            }));
+        }
+        IPage<AttrEntity> page = this.page(
+                new Query<AttrEntity>().getPage(params),
+                queryWrapper
+        );
+
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrRespVo> respVos = records.stream().map(attrEntity -> {
+            AttrRespVo attrRespVo = new AttrRespVo();
+            BeanUtils.copyProperties(attrEntity, attrRespVo);
+
+            // 设置分类和分组的名字。
+            AttrAttrgroupRelationEntity attrId =
+                    relationDao.selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id",
+                            attrEntity.getAttrId()));
+
+            if (attrId != null) {
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrId.getAttrGroupId());
+                attrRespVo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getAttrId());
+            if (categoryEntity != null) {
+                attrRespVo.setCatelogName(categoryEntity.getName());
+            }
+            return attrRespVo;
+        }).collect(Collectors.toList());
+
+        pageUtils.setList(respVos);
+        return pageUtils;
     }
 
 }
